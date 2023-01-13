0e6b0820a8338cf4608d18d0f42c3e598cef2402
FantomeBeignet <himself@fantomebeig.net>
2023-01-13 20:40:48 +0100

    [+] custom blink animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				blink: {
+					"0%, 100%": { opacity: 0 },
+					"50%": { opacity: 1 },
+				},
+			},
+			animation: {
+				blink: "blink 1.2s steps(1, start) infinite",
+			},
+		},
 	},
 	plugins: [],
 }
