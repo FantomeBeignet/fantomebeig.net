71d3fdbf9edde2deab7aa347d58e5b31c107b12b
FantomeBeignet <himself@fantomebeig.net>
2023-01-14 14:58:33 +0100

    [+] made sidebar responsive

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,3 +1,10 @@
+<script>
+  let isMenuOpen = false;
+  function toggleMenu() {
+    isMenuOpen = !isMenuOpen;
+  }
+</script>
+
 <nav class="navbar">
   <h3 class="logotype">
     <span class="logotype__highlight">F</span>antome<span
@@ -10,29 +17,35 @@
     <li class="nav-item">// projects</li>
     <li class="nav-item">// contact</li>
   </ul>
+  <button on:click={toggleMenu} class="menu-button">//</button>
+  <div class={isMenuOpen ? "sidebar sidebar__open" : "sidebar sidebar__closed"}>
+    <div class="sidebar-top">
+      <button on:click={toggleMenu} class="sidebar-close">//</button>
+    </div>
+    <ul class="sidebar-links">
+      <li class="sidebar-link">// home</li>
+      <li class="sidebar-link">// about</li>
+      <li class="sidebar-link">// projects</li>
+      <li class="sidebar-link">// contact</li>
+    </ul>
+  </div>
 </nav>
 
 <style lang="scss">
   .navbar {
+    font-family: "Fira Code", monospace;
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: center;
     width: 100%;
-    padding: 1.5rem 0;
-
-    &:after {
-    content: "";
-    flex: 1;
-    }
+    padding: 1.5rem;
   }
 
   .logotype {
     flex: 1;
-    font-family: "Fira Code", monospace;
-    font-size: 1.5rem;
-    line-height: 2rem;
-    padding: 0 1.5rem;
+    font-size: 1.25rem;
+    line-height: 1.75rem;
     color: var(--color-text);
 
     .blink {
@@ -45,7 +58,7 @@
   }
 
   .nav-inner {
-    display: flex;
+    display: none;
     flex-direction: row;
     align-items: center;
     justify-content: center;
@@ -57,19 +70,110 @@
   }
 
   .nav-item {
-    font-family: "Fira Code", monospace;
     font-size: 1.25rem;
     line-height: 1.75rem;
     color: var(--color-text);
     transition: color 0.2s ease-in-out;
   }
 
+  .menu-button {
+    display: flex;
+    color: var(--color-text);
+    font-size: 1.25rem;
+    line-height: 1.75rem;
+  }
+
+  .sidebar {
+    transition: opacity 0.15s ease-in-out;
+  }
+
+  .sidebar__closed {
+    opacity: 0;
+    pointer-events: none;
+    height: 0;
+    width: 0;
+    overflow: hidden;
+  }
+
+  .sidebar__open {
+    opacity: 1;
+    pointer-events: auto;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    background-color: var(--color-text);
+    z-index: 1;
+    padding: 1.5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 8rem;
+
+    &:after {
+      content: "";
+      flex: 1;
+    }
+
+    .sidebar-top {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: end;
+      width: 100%;
+
+      .sidebar-close {
+        background-color: transparent;
+        color: var(--color-background);
+        font-size: 1.25rem;
+        line-height: 1.75rem;
+        cursor: pointer;
+      }
+    }
+
+    .sidebar-links {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: start;
+      justify-content: center;
+      gap: 1rem;
+
+      .sidebar-link {
+        font-size: 1.25rem;
+        line-height: 1.75;
+        color: var(--color-background);
+      }
+    }
+  }
+
   @keyframes blink {
-    0%, 100% {
+    0%,
+    100% {
       opacity: 0;
     }
     50% {
       opacity: 1;
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    .navbar::after {
+      content: "";
+      flex: 1;
+    }
+    .logotype {
+      font-size: 1.5rem;
+      line-height: 2rem;
+    }
+    .nav-inner {
+      display: flex;
+    }
+
+    .menu-button {
+      display: none;
     }
   }
 </style>
