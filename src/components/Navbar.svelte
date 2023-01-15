807ffe56691984cbfdbcf9db96b88e50c1771da9
FantomeBeignet <himself@fantomebeig.net>
2023-01-15 15:26:35 +0100

    [~] refactor to remove useless elements

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -5,36 +5,26 @@
   }
 </script>
 
-<nav class="navbar">
+<nav class={`navbar ${isMenuOpen ? "sidebar__open" : "sidebar__closed"}`}>
   <h3 class="logotype">
     <span class="logotype__highlight">F</span>antome<span
       class="logotype__highlight">B</span
     >eignet<span class="blink">_</span>
   </h3>
-  <ul class="nav-inner">
+  <ul class="nav-inner" aria-expanded={isMenuOpen ? "true" : "false"}>
     <li class="nav-item">// home</li>
     <li class="nav-item">// about</li>
     <li class="nav-item">// projects</li>
     <li class="nav-item">// contact</li>
   </ul>
   <button on:click={toggleMenu} class="menu-button">//</button>
-  <div class={isMenuOpen ? "sidebar sidebar__open" : "sidebar sidebar__closed"}>
-    <div class="sidebar-top">
-      <button on:click={toggleMenu} class="sidebar-close">//</button>
-    </div>
-    <ul class="sidebar-links">
-      <li class="sidebar-link">// home</li>
-      <li class="sidebar-link">// about</li>
-      <li class="sidebar-link">// projects</li>
-      <li class="sidebar-link">// contact</li>
-    </ul>
-  </div>
 </nav>
 
 <style lang="scss">
   .navbar {
     font-family: "Fira Code", monospace;
     display: flex;
+    position: relative;
     flex-direction: row;
     align-items: center;
     justify-content: center;
@@ -58,11 +48,21 @@
   }
 
   .nav-inner {
-    display: none;
-    flex-direction: row;
-    align-items: center;
+    z-index: 1;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
     justify-content: center;
-    gap: 4rem;
+    gap: 1.5rem;
+    background-color: var(--color-text);
+    padding: 1.5rem;
+    opacity: 0;
+    transition: opacity 0.2s ease-in-out;
 
     &:hover > .nav-item:not(:hover) {
       color: var(--color-background-light);
@@ -77,85 +77,25 @@
   }
 
   .menu-button {
+    z-index: 9999;
     display: flex;
     color: var(--color-text);
     font-size: 1.25rem;
     line-height: 1.75rem;
   }
 
-  .sidebar {
-    transition: opacity 0.15s ease-in-out;
-  }
-
-  .sidebar__closed {
+  .sidebar__closed > .nav-inner {
     opacity: 0;
-    pointer-events: none;
-    height: 0;
-    width: 0;
-    overflow: hidden;
   }
 
   .sidebar__open {
-    opacity: 1;
-    pointer-events: auto;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100vh;
-    background-color: var(--color-text);
-    z-index: 1;
-    padding: 1.5rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 8rem;
-
-    &:after {
-      content: "";
-      flex: 1;
-    }
-
-    .sidebar-top {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: end;
-      width: 100%;
-
-      .sidebar-close {
-        background-color: transparent;
-        color: var(--color-background);
-        font-size: 1.25rem;
-        line-height: 1.75rem;
-        cursor: pointer;
-      }
-    }
-
-    .sidebar-links {
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      align-items: start;
-      justify-content: center;
-      gap: 1rem;
-
-      .sidebar-link {
-        font-size: 1.25rem;
-        line-height: 1.75;
-        color: var(--color-background);
-      }
-    }
-  }
-
-  @keyframes blink {
-    0%,
-    100% {
-      opacity: 0;
-    }
-    50% {
+    .nav-inner {
       opacity: 1;
+    }
+
+    .nav-item,
+    .menu-button {
+      color: var(--color-background-dark);
     }
   }
 
@@ -168,12 +108,31 @@
       font-size: 1.5rem;
       line-height: 2rem;
     }
+
     .nav-inner {
-      display: flex;
+      opacity: 1 !important;
+      height: 100%;
+      background-color: transparent;
+      color: var(--color-text);
+      inset: unset;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      gap: 4rem;
     }
 
     .menu-button {
       display: none;
+    }
+  }
+
+  @keyframes blink {
+    0%,
+    100% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
     }
   }
 </style>
