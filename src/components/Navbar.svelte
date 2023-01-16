435ddeeafacc14ef1511bfe10f1d7a8655f13b8b
FantomeBeignet <himself@fantomebeig.net>
2023-01-16 01:34:57 +0100

    [+] switched to BEM conventions

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -5,19 +5,19 @@
   }
 </script>
 
-<nav class={`navbar ${isMenuOpen ? "sidebar__open" : "sidebar__closed"}`}>
-  <h3 class="logotype">
-    <span class="logotype__highlight">F</span>antome<span
-      class="logotype__highlight">B</span
-    >eignet<span class="blink">_</span>
+<nav class={`navbar ${isMenuOpen ? "sidebar--open" : "sidebar--closed"}`}>
+  <h3 class="navbar__logo">
+    <span class="navbar__logo--highlight">F</span>antome<span
+      class="navbar__logo--highlight">B</span
+    >eignet<span class="navbar__logo--blink">_</span>
   </h3>
-  <ul class="nav-inner" aria-expanded={isMenuOpen ? "true" : "false"}>
-    <li class="nav-item"><a href="/">// home</a></li>
-    <li class="nav-item">// about</li>
-    <li class="nav-item">// projects</li>
-    <li class="nav-item">// contact</li>
+  <ul class="navbar__links" aria-expanded={isMenuOpen ? "true" : "false"}>
+    <li class="navbar__link"><a href="/">// home</a></li>
+    <li class="navbar__link">// about</li>
+    <li class="navbar__link">// projects</li>
+    <li class="navbar__link">// contact</li>
   </ul>
-  <button on:click={toggleMenu} class="menu-button">//</button>
+  <button on:click={toggleMenu} class="navbar__button">//</button>
 </nav>
 
 <style lang="scss">
@@ -30,107 +30,109 @@
     justify-content: center;
     width: 100%;
     padding: 1.5rem;
-  }
 
-  .logotype {
-    flex: 1;
-    font-size: 1.25rem;
-    line-height: 1.75rem;
-    color: var(--color-text);
+    &__logo {
+      flex: 1;
+      font-size: 1.25rem;
+      line-height: 1.75rem;
+      color: var(--color-text);
 
-    .blink {
-      animation: blink 1.2s steps(1, start) infinite;
-    }
-  }
+      &--blink {
+        animation: blink 1.2s steps(1, start) infinite;
+      }
 
-  .logotype__highlight {
-    color: var(--color-accent);
-  }
-
-  .nav-inner {
-    z-index: 1;
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: center;
-    gap: 1.5rem;
-    background-color: var(--color-text);
-    padding: 1.5rem;
-    opacity: 0;
-    transition: opacity 0.2s ease-in-out;
-
-    &:hover > .nav-item:not(:hover) {
-      color: var(--color-background-light);
-    }
-  }
-
-  .nav-item {
-    font-size: 1.25rem;
-    line-height: 1.75rem;
-    color: var(--color-text);
-    transition: color 0.2s ease-in-out;
-  }
-
-  .menu-button {
-    z-index: 9999;
-    display: flex;
-    color: var(--color-text);
-    font-size: 1.25rem;
-    line-height: 1.75rem;
-  }
-
-  .sidebar__closed > .nav-inner {
-    opacity: 0;
-  }
-
-  .sidebar__open {
-    .nav-inner {
-      opacity: 1;
+      &--highlight {
+        color: var(--color-accent);
+      }
     }
 
-    .nav-item,
-    .menu-button {
-      color: var(--color-background-dark);
+    &__links {
+      z-index: 1;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100vh;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: center;
+      gap: 1.5rem;
+      background-color: var(--color-text);
+      padding: 1.5rem;
+      opacity: 0;
+      transition: opacity 0.2s ease-in-out;
+
+      &:hover > li:not(:hover) {
+        color: var(--color-background-light);
+      }
+    }
+
+    &__link {
+      font-size: 1.25rem;
+      line-height: 1.75rem;
+      color: var(--color-text);
+      transition: color 0.2s ease-in-out;
+    }
+
+    &__button {
+      z-index: 9999;
+      display: flex;
+      color: var(--color-text);
+      font-size: 1.25rem;
+      line-height: 1.75rem;
+    }
+
+    &.sidebar--closed > &__links {
+      opacity: 0;
+    }
+
+    &.sidebar--open {
+      .navbar__links {
+        opacity: 1;
+      }
+
+      .navbar__link,
+      .navbar__button {
+        color: var(--color-background-dark);
+      }
     }
   }
 
   @media screen and (min-width: 1024px) {
     .navbar {
       padding: 2rem;
-    }
-    .navbar::after {
-      content: "";
-      flex: 1;
-    }
-    .logotype {
-      font-size: 1.5rem;
-      line-height: 2rem;
-    }
 
-    .nav-inner {
-      opacity: 1 !important;
-      height: 100%;
-      background-color: transparent;
-      color: var(--color-text);
-      inset: unset;
-      flex-direction: row;
-      align-items: center;
-      justify-content: center;
-      gap: 4rem;
-    }
+      &::after {
+        content: "";
+        flex: 1;
+      }
 
-    .nav-item {
-      font-size: 1.5rem;
-      line-height: 2rem;
-    }
+      &__logo {
+        font-size: 1.5rem;
+        line-height: 2rem;
+      }
 
-    .menu-button {
-      display: none;
+      &__links {
+        opacity: 1 !important;
+        height: 100%;
+        background-color: transparent;
+        color: var(--color-text);
+        inset: unset;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        gap: 4rem;
+      }
+
+      &__link {
+        font-size: 1.5rem;
+        line-height: 2rem;
+      }
+
+      &__button {
+        display: none;
+      }
     }
   }
 
