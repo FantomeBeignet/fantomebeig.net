e3c761ab74b21a993373446b639948a68a0a7706
FantomeBeignet <himself@fantomebeig.net>
2023-01-18 22:57:46 +0100

    [+] site url in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.fantomebeig.net",
   integrations: [svelte()],
   vite: {
     resolve: {
