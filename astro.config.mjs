50b7aba0df840885fe1aa7fee55a63ec6f7e5665
FantomeBeignet <himself@fantomebeig.net>
2023-01-18 20:36:49 +0100

    [+] going full scss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,24 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { fileURLToPath, URL } from "url";
 
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  vite: {
+    resolve: {
+      alias: {
+        "@styles": fileURLToPath(new URL("./src/styles", import.meta.url)),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@styles/style.scss" as *;`,
+        },
+      },
+    },
+  },
 });
