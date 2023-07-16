1e9fdf4233b9a32358b04906fe64430ec2ea2912
Tom Béné <himself@fantomebeig.net>
2023-07-16 22:33:18 +0200

    feat: added reading time to articles

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,16 @@ import mdx from '@astrojs/mdx';
 import AutoImport from 'astro-auto-import';
 import MDXCodeBlocks, { mdxCodeBlockAutoImport } from 'astro-mdx-code-blocks';
 
+import { remarkReadingTime } from './src/lib/remark-reading-time.mjs';
+
 // https://astro.build/config
 export default defineConfig({
 	markdown: {
 		syntaxHighlight: 'shiki',
 		shikiConfig: {
-			wrap: true,
-		}
+			wrap: true
+		},
+		remarkPlugins: [remarkReadingTime]
 	},
 	integrations: [
 		AutoImport({
