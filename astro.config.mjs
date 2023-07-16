e6f89f3bec2d0b48cf32bc82f698c0159952d00d
Tom Béné <himself@fantomebeig.net>
2023-07-16 18:22:48 +0200

    feat: custom codeblock component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,26 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
+
+import AutoImport from 'astro-auto-import';
+import MDXCodeBlocks, { mdxCodeBlockAutoImport } from 'astro-mdx-code-blocks';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), mdx()]
+	markdown: {
+		syntaxHighlight: 'shiki',
+		shikiConfig: {
+			wrap: true,
+		}
+	},
+	integrations: [
+		AutoImport({
+			imports: [mdxCodeBlockAutoImport('src/components/CodeBlock.astro')]
+		}),
+		MDXCodeBlocks(),
+		svelte(),
+		tailwind(),
+		mdx()
+	]
 });
