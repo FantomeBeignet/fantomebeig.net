6f8ca0535b80a120693f67609ef046a05da3502c
Tom Béné <himself@fantomebeig.net>
2023-07-19 18:12:07 +0200

    feat: moved series to data collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,22 @@
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection, reference } from 'astro:content';
+
+const seriesCollection = defineCollection({
+	type: 'data',
+	schema: z.object({
+		title: z.string()
+	})
+});
 
 const blogCollection = defineCollection({
 	type: 'content',
 	schema: z.object({
 		title: z.string(),
-		series: z.string().optional(),
+		series: z.optional(reference('series')),
 		publishDate: z.date().optional()
 	})
 });
 
 export const collections = {
+	series: seriesCollection,
 	blog: blogCollection
 };
